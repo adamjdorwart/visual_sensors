0a6c09dc7bb1bd2ec41a2811a9f59e56fb26329f
Adam Dorwart <c_dorwart@braincorporation.com>
2015-08-14 11:01:12 -0700

    Baseline sensor plate complete. Need to add parameter adjustment.

--- src/three_rgbd_plate.py
+++ src/three_rgbd_plate.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 	left_rgbd_base_name = rospy.get_param("~left_rgbd_base_name","left_rgbd")
 	left_rgbd_calibration = rospy.get_param("~left_rgbd_calibration","")
 	left_rgbd_position = rospy.get_param("~left_rgbd_position",(-0.1275,0.2275,0.0))
-	left_rgbd_rotation = rospy.get_param("~left_rgbd_rotation",(-1.57079633 ,0,-1.57079633))
+	left_rgbd_rotation = rospy.get_param("~left_rgbd_rotation",(-1.57079633,0,-1.57079633 + 0.698131701))
 	left_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(left_rgbd_rotation[0],
 																		left_rgbd_rotation[1],
 																		left_rgbd_rotation[2])
@@ -26,8 +26,8 @@ if __name__ == '__main__':
 	center_rgbd_serial = rospy.get_param("~center_rgbd_serial_id","")
 	center_rgbd_base_name = rospy.get_param("~center_rgbd_base_name","center_rgbd")
 	center_rgbd_calibration = rospy.get_param("~center_rgbd_calibration","")
-	center_rgbd_position = rospy.get_param("~center_rgbd_position",(0.0,0.0,0.0))
-	center_rgbd_rotation = rospy.get_param("~center_rgbd_rotation",(-0.872664626,0.0,-1.57079633))
+	center_rgbd_position = rospy.get_param("~center_rgbd_position",(0,0,0))
+	center_rgbd_rotation = rospy.get_param("~center_rgbd_rotation",(-1.57079633,0,-1.57079633))
 	center_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(center_rgbd_rotation[0],
 																		center_rgbd_rotation[1],
 																		center_rgbd_rotation[2])
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 	right_rgbd_base_name = rospy.get_param("~right_rgbd_base_name","right_rgbd")
 	right_rgbd_calibration = rospy.get_param("~right_rgbd_calibration","")
 	right_rgbd_position = rospy.get_param("~right_rgbd_position",(-0.1275,-0.2275,0.0))
-	right_rgbd_rotation = rospy.get_param("~right_rgbd_rotation",(-2.26892803,0.0,-1.57079633))
+	right_rgbd_rotation = rospy.get_param("~right_rgbd_rotation",(-1.57079633,0,-1.57079633 - 0.698131701))
 	right_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(right_rgbd_rotation[0],
 																		right_rgbd_rotation[1],
 																		right_rgbd_rotation[2])
@@ -53,17 +53,17 @@ if __name__ == '__main__':
 		left_rgbd_tf.sendTransform(left_rgbd_position,
 			left_rgbd_rotation_qt,
 			rospy.Time.now(),
-			plate_base_name,
-			left_rgbd_base_name + "_link")
+			left_rgbd_base_name + "_link",
+			plate_base_name)
 		center_rgbd_tf.sendTransform(center_rgbd_position,
 			center_rgbd_rotation_qt,
 			rospy.Time.now(),
-			plate_base_name,
-			center_rgbd_base_name + "_link")
+			center_rgbd_base_name + "_link",
+			plate_base_name)
 		right_rgbd_tf.sendTransform(right_rgbd_position,
 			right_rgbd_rotation_qt,
 			rospy.Time.now(),
-			plate_base_name,
-			right_rgbd_base_name + "_link")
+			right_rgbd_base_name + "_link",
+			plate_base_name)
 
 		rate.sleep()
