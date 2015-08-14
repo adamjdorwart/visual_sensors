07f79646415b9a89e4287584afa85f53bc6b0c54
Adam Dorwart <c_dorwart@braincorporation.com>
2015-08-14 12:44:06 -0700

    Add plate specific configuration, with yaw and pitch arguments.

--- src/three_rgbd_plate.py
+++ src/three_rgbd_plate.py
@@ -4,6 +4,7 @@ import roslib
 roslib.load_manifest("visual_sensors")
 import rospy
 import tf
+import math
 
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
@@ -14,32 +15,30 @@ from dynamic_reconfigure.server import Server as DynamicReconfigureServer
 if __name__ == '__main__':
 	rospy.init_node('three_rgbd_plate')
 
-	left_rgbd_serial = rospy.get_param("~left_rgbd_serial_id","")
 	left_rgbd_base_name = rospy.get_param("~left_rgbd_base_name","left_rgbd")
-	left_rgbd_calibration = rospy.get_param("~left_rgbd_calibration","")
-	left_rgbd_position = rospy.get_param("~left_rgbd_position",(-0.1275,0.2275,0.0))
-	left_rgbd_rotation = rospy.get_param("~left_rgbd_rotation",(-1.57079633,0,-1.57079633 + 0.698131701))
-	left_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(left_rgbd_rotation[0],
+	left_yaw_setting = rospy.get_param("~left_yaw_setting",50.0)
+	left_pitch_setting = rospy.get_param("~left_pitch_setting",0.0)
+	left_rgbd_position = rospy.get_param("~left_rgbd_position",(-0.1275,0.23,0.0))
+	left_rgbd_rotation = rospy.get_param("~left_rgbd_rotation",(-1.57079633 ,0,-1.57079633))
+	left_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(left_rgbd_rotation[0] + math.radians(left_pitch_setting),
 																		left_rgbd_rotation[1],
-																		left_rgbd_rotation[2])
+																		left_rgbd_rotation[2] + math.radians(left_yaw_setting))
 
-	center_rgbd_serial = rospy.get_param("~center_rgbd_serial_id","")
 	center_rgbd_base_name = rospy.get_param("~center_rgbd_base_name","center_rgbd")
-	center_rgbd_calibration = rospy.get_param("~center_rgbd_calibration","")
 	center_rgbd_position = rospy.get_param("~center_rgbd_position",(0,0,0))
 	center_rgbd_rotation = rospy.get_param("~center_rgbd_rotation",(-1.57079633,0,-1.57079633))
 	center_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(center_rgbd_rotation[0],
 																		center_rgbd_rotation[1],
 																		center_rgbd_rotation[2])
 
-	right_rgbd_serial = rospy.get_param("~right_rgbd_serial_id","")
 	right_rgbd_base_name = rospy.get_param("~right_rgbd_base_name","right_rgbd")
-	right_rgbd_calibration = rospy.get_param("~right_rgbd_calibration","")
-	right_rgbd_position = rospy.get_param("~right_rgbd_position",(-0.1275,-0.2275,0.0))
-	right_rgbd_rotation = rospy.get_param("~right_rgbd_rotation",(-1.57079633,0,-1.57079633 - 0.698131701))
-	right_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(right_rgbd_rotation[0],
+	right_yaw_setting = rospy.get_param("~right_yaw_setting",50.0)
+	right_pitch_setting = rospy.get_param("~right_pitch_setting",0.0)
+	right_rgbd_position = rospy.get_param("~right_rgbd_position",(-0.1275,-0.23,0.0))
+	right_rgbd_rotation = rospy.get_param("~right_rgbd_rotation",(-1.57079633 ,0,-1.57079633 ))
+	right_rgbd_rotation_qt = tf.transformations.quaternion_from_euler(right_rgbd_rotation[0] + math.radians(right_pitch_setting),
 																		right_rgbd_rotation[1],
-																		right_rgbd_rotation[2])
+																		right_rgbd_rotation[2] - math.radians(right_yaw_setting))
 
 	plate_base_name = rospy.get_param("~plate_base_name","three_rgbd_plate")
 
